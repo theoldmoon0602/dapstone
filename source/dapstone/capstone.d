ed311c9e8d31997d99f76b9e6913ae82a44cd041
furutsuki <theoldmoon0602@gmail.com>
2018-11-06 12:41:53 +0900

    update

--- source/dapstone/capstone.d
+++ source/dapstone/capstone.d
@@ -1,18 +1,8 @@
 module dapstone.capstone;
 
 import std.exception;
+import std.string;
 import capstone.capstone;
-
-string fromStringz(S)(S s) {
-  import std.conv;
-  import core.stdc.string;
-  static if (is(S == char*)) {
-    auto len = s.strlen();
-    return s[0..len].to!string();
-  } else {
-    return fromStringz(s.ptr);
-  }
-}
 
 static class DapstoneException : Exception
 {
@@ -48,8 +38,8 @@ class Capstone {
       foreach (i; 0..count) {
         irs ~= Instruction(
           insn[i].address,
-          insn[i].mnemonic.fromStringz(),
-          insn[i].op_str.fromStringz(),
+          cast(string)(insn[i].mnemonic.ptr.fromStringz()),
+          cast(string)(insn[i].op_str.ptr.fromStringz()),
           insn[i].bytes[0..insn[i].size],
         );
       }
